96621aba5f91391319fc42c74cef43f0b352c920
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-22 01:30:15 +0200

    Created a password generator

---
+++ passwordgenerator.js
@@ -0,0 +1,111 @@
+const characters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "+",
+  "-",
+  "=",
+  "[",
+  "]",
+  "{",
+  "}",
+  "|",
+  ";",
+  ":",
+  ",",
+  ".",
+  "<",
+  ">",
+  "/",
+  "?",
+  "~",
+];
+
+const button = document.querySelector("button");
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
+
+button.addEventListener("click", (event) => {
+  password1.textContent = "";
+  password2.textContent = "";
+
+  for (let i = 0; i < 15; i++) {
+    password1.textContent +=
+      characters[Math.floor(Math.random() * characters.length)];
+  }
+
+  for (let i = 0; i < 15; i++) {
+    password2.textContent +=
+      characters[Math.floor(Math.random() * characters.length)];
+  }
+});
