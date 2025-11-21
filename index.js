2fee0e28537eadac382d88076c30ff16831c1673
CharPet <137006290+CharPet@users.noreply.github.com>
2025-11-21 23:44:30 +0200

    Solved Exercises. Added Emoji Fighter

---
+++ index.js
@@ -0,0 +1,90 @@
+let person = {
+  name: "Pepe",
+  age: 23,
+  country: "Pepeland",
+};
+
+function logData() {
+  console.log(
+    person.name +
+      " is " +
+      person.age +
+      " years old and lives in " +
+      person.country
+  );
+}
+
+logData();
+
+// -----------------------------------------
+
+let age = 15;
+
+if (age < 6) {
+  console.log("free");
+} else if (age <= 17) {
+  console.log("child discount");
+} else if (age <= 26) {
+  console.log("student discount");
+} else if (age <= 66) {
+  console.log("full price");
+} else {
+  console.log("senior citizen discount");
+}
+
+// -----------------------------------------
+
+let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"];
+
+for (let i = 0; i < largeCountries.length; i++) {
+  console.log("- " + largeCountries[i]);
+}
+
+// -----------------------------------------
+
+let largeCountries2 = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"];
+
+console.log(largeCountries2);
+
+largeCountries2.shift();
+largeCountries2.pop();
+largeCountries2.unshift("China");
+largeCountries2.push("Pakistan");
+
+console.log(largeCountries2);
+
+// -----------------------------------------
+
+let dayOfMonth = 31;
+let weekday = "Friday";
+
+if (dayOfMonth === 13 && weekday === "Friday") {
+  console.log("[Spooky Emoji]");
+}
+
+// -----------------------------------------
+
+let hands = ["rock", "paper", "scissor"];
+
+function getHand() {
+  let randomIndex = Math.floor(Math.random() * 3);
+  return hands[randomIndex];
+}
+
+console.log(getHand());
+
+// -----------------------------------------
+
+let fighters = ["A", "B", "C", "D"];
+
+let stageEl = document.getElementById("stage");
+let fightButton = document.getElementById("fightButton");
+
+fightButton.addEventListener("click", function () {
+  let randomFighter1 = fighters[Math.floor(Math.random() * fighters.length)];
+  let randomFighter2 = fighters[Math.floor(Math.random() * fighters.length)];
+  stageEl.textContent = randomFighter1 + " vs " + randomFighter2;
+  console.log("click");
+});
+
+// -----------------------------------------
